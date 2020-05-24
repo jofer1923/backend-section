5d9ca89cb30921343df4fe53c3d78be62c72bd5c
Jose Fernando <jofer1923@gmail.com>
2020-05-23 20:51:02 -0500

    [FIX] UserService

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 let _userService = null;
 class UserController {
   constructor({ UserService }) {
-    _userService = UserController;
+    _userService = UserService;
   }
   async get(req, res) {
     const { userId } = req.params;
