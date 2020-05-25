d095c8f0c4de11523fba2b7532659f950586e8b9
Jose Fernando <jofer1923@gmail.com>
2020-05-25 06:56:07 -0500

    [ADD] swagger implemented

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,11 +9,7 @@ const { CACHE_TIME } = require("../helpers");
 module.exports = function ({ UserController }) {
   const router = Router();
 
-  router.get(
-    "",
-    [AuthMiddleware, ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)],
-    UserController.getAll
-  );
+  router.get("", [ParseIntMiddleware], UserController.getAll);
   router.get("/:userId", UserController.get);
   router.patch("/:userId", UserController.update);
   router.delete("/:userId", UserController.delete);
